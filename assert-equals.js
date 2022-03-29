083800ea1b4b22edea1ccd0bc2fc6c5746b48749
Bereketmebrahtu <mebrahtubereket@gmail.com>
2022-03-29 09:43:52 +0100

    added asserEqual tests

--- assert-equals.js
+++ assert-equals.js
@@ -1,3 +1,20 @@
-function assertEquals(expect, actual) {}
+function assertEquals(expect, actual) {
+    if(expect === actual) {
+        return actual
+    }
+    else {
+        return `expected ${expect} but got ${actual}`
+    }
+
+}
+
+
+const checkEqual = (value) => {
+    return typeof(value)
+}
+
+let actual = checkEqual(null)
+let expected = 'primitive'
+assertEquals(expect, actual)
 
 module.exports = assertEquals
--- assert-equals.spec.js
+++ assert-equals.spec.js
@@ -3,7 +3,7 @@ const assertEquals = require('./assert-equals')
 describe('assertEquals', () => {
   describe('when expected and actual are the same string', () => {
     it('returns without throwing an error', () => {
-      expect(() => assertEquals('abc', 'abc')).not.toThrow()
+      expect(() => assertEquals(expect, actual)).not.toThrow()
     })
   })
 
