9f5caabdb5ae34aa28768dbd27bdddb49853554e
Bereketmebrahtu <mebrahtubereket@gmail.com>
2022-03-29 11:04:35 +0100

    Added more tests

--- assert-equals.js
+++ assert-equals.js
@@ -1,20 +1,32 @@
 function assertEquals(expect, actual) {
-    if(expect === actual) {
-        return actual
+    if (expect === actual) {
+        return ` Passed: ${expect} === ${actual}`
     }
     else {
-        return `expected ${expect} but got ${actual}`
+        return `Failed: expected ${expect} but got ${actual}`
     }
 
 }
-
-
 const checkEqual = (value) => {
-    return typeof(value)
+    return typeof (value)
 }
 
 let actual = checkEqual(null)
-let expected = 'primitive'
+let expect = 'null'
 assertEquals(expect, actual)
+
+let actual1 = checkEqual({})
+let expect1 = 'object'
+assertEquals(expect1, actual1)
+
+
+const checkLength = (str) => {
+    return str.length
+}
+
+
+let actual2 = checkLength('Hello')
+let expect2 = 3;
+assertEquals(expect2, actual2)
 
 module.exports = assertEquals
--- assert-equals.spec.js
+++ assert-equals.spec.js
@@ -1,13 +1,26 @@
 const assertEquals = require('./assert-equals')
 
 describe('assertEquals', () => {
-  describe('when expected and actual are the same string', () => {
+  describe('when expected and actual are the same data type', () => {
     it('returns without throwing an error', () => {
-      expect(() => assertEquals(expect, actual)).not.toThrow()
+      expect((expect, actual) => assertEquals(expect === actual)).not.toThrow()
     })
   })
 
-  describe('when expected and actual are different strings', () => {
+  describe('when expected and actual are different data type', () => {
+    it.todo('throws an error')
+  })
+})
+
+
+describe('assertEquals', () => {
+  describe('when expected and actual are the same string length', () => {
+    it('returns without throwing an error', () => {
+      expect(() => assertEquals()).not.toThrow()
+    })
+  })
+
+  describe('when expected and actual are different string length', () => {
     it.todo('throws an error')
   })
 })
